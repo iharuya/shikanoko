ce6f7e77e8c396bd147191652159d1e8132249bd
iharuya <i4haruya@gmail.com>
2024-08-04 13:48:52 +0900

    follow matrix P

--- src/App.tsx
+++ src/App.tsx
@@ -1,183 +1,179 @@
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
-import { StateNode } from './StateNode';
+import { FC, useCallback, useEffect, useRef, useState } from "react"
+import { StateNode } from "./StateNode"
+import { sleep } from "./lib/time"
 
-const states = ["b", "し", "か", "の", "こ", "た", "ん"] as const;
-type State = typeof states[number];
+const states = ["b", "し", "か", "の", "こ", "た", "ん"] as const
+type State = (typeof states)[number]
 const stateToAudioName = {
-  b: 'beat',
-  し: 'shi',
-  か: 'ka',
-  の: 'no-1',
-  こ: 'ko-1',
-  た: 'tan',
-  ん: 'n'
+	b: "beat",
+	し: "shi",
+	か: "ka",
+	の: "no-1",
+	こ: "ko-1",
+	た: "tan",
+	ん: "n"
 } satisfies Record<State, string>
 
 const P = [
-  [1 / 2, 1 / 2, 0, 0, 0, 0, 0],
-  [0, 0, 1 / 2, 0, 0, 1 / 2, 0],
-  [0, 0, 0, 1, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0],
-  [0, 1 / 4, 0, 1 / 2, 1 / 4, 0, 0],
-  [0, 0, 0, 0, 0, 0, 1],
-  [1 / 2, 0, 0, 0, 0, 1 / 2, 0]
+	[1 / 2, 1 / 2, 0, 0, 0, 0, 0],
+	[0, 0, 1 / 2, 0, 0, 1 / 2, 0],
+	[0, 0, 0, 1, 0, 0, 0],
+	[0, 0, 0, 0, 1, 0, 0],
+	[0, 1 / 4, 0, 1 / 2, 1 / 4, 0, 0],
+	[0, 0, 0, 0, 0, 0, 1],
+	[1 / 2, 0, 0, 0, 0, 1 / 2, 0]
 ] as const
 
 export const App: FC = () => {
-  const audioContextRef = useRef<AudioContext | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [currentState, setCurrentState] = useState<State>("b");
-  const [audioBuffers, setAudioBuffers] = useState<Record<State, AudioBuffer> | null>(null);
+	const audioContextRef = useRef<AudioContext | null>(null)
+	const [isPlaying, setIsPlaying] = useState(false)
+	const [currentState, setCurrentState] = useState<State>("b")
+	const [audioBuffers, setAudioBuffers] = useState<Record<
+		State,
+		AudioBuffer
+	> | null>(null)
+	const [steps, setSteps] = useState(0)
 
-  const loadAudioFiles = useCallback(async () => {
-    setAudioBuffers(null);
-    const audioBuffers = {} as Record<State, AudioBuffer>;
-    for (const state of states) {
-      const path = `/audio/${stateToAudioName[state]}.mp3`;
-      try {
-        const buffer = await loadAudioFile(path);
-        if (!buffer) throw new Error("No buffer");
-        audioBuffers[state] = buffer
-      } catch (error) {
-        console.error(error);
-        throw new Error(`Failed to load audio file: ${path}`);
-      }
-    }
-    setAudioBuffers(audioBuffers);
-  }, [])
+	const loadAudioFiles = useCallback(async () => {
+		setAudioBuffers(null)
+		const audioBuffers = {} as Record<State, AudioBuffer>
+		for (const state of states) {
+			const path = `/audio/${stateToAudioName[state]}.mp3`
+			try {
+				const buffer = await loadAudioFile(path)
+				if (!buffer) throw new Error("No buffer")
+				audioBuffers[state] = buffer
+			} catch (error) {
+				console.error(error)
+				throw new Error(`Failed to load audio file: ${path}`)
+			}
+		}
+		setAudioBuffers(audioBuffers)
+	}, [])
 
-  const loadAudioFile = useCallback(async (url: string) => {
-    if (!audioContextRef.current) return
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    return await audioContextRef.current.decodeAudioData(arrayBuffer);
-  }, [])
+	const loadAudioFile = useCallback(async (url: string) => {
+		if (!audioContextRef.current) return
+		const response = await fetch(url)
+		const arrayBuffer = await response.arrayBuffer()
+		return await audioContextRef.current.decodeAudioData(arrayBuffer)
+	}, [])
 
-  // const transitionToNextState = useCallback(() => {
-  //   const currentIndex = states.indexOf(currentState);
-  //   let nextStateIndex = 0;
-  //   const randomValue = Math.random();
-  //   let cumulativeProbability = 0;
-  //   for (let i = 0; i < 7; i++) {
-  //     cumulativeProbability += P[currentIndex][i];
-  //     if (randomValue < cumulativeProbability) {
-  //       nextStateIndex = i;
-  //       break;
-  //     }
-  //   }
-  //   const nextState = states[nextStateIndex];
-  //   setCurrentState(nextState);
-  //   playSound(nextState);
-  // }, [currentState, playSound]);
+	const next = useCallback(async () => {
+		console.log("run next")
+		if (!audioBuffers || !audioContextRef.current) return
+		const currentIndex = states.indexOf(currentState)
+		let nextStateIndex = 0
+		const randomValue = Math.random()
+		let cumulativeProbability = 0
+		for (let i = 0; i < 7; i++) {
+			cumulativeProbability += P[currentIndex][i]
+			if (randomValue < cumulativeProbability) {
+				nextStateIndex = i
+				break
+			}
+		}
+		const nextState = states[nextStateIndex]
+		console.log(nextState)
+		setCurrentState(nextState)
 
-  // const playSound = useCallback(
-  //   async (state: State) => {
-  //     if (!audioContextRef.current || !audioBuffers) return;
-  //     console.log(state)
-  //     const source = audioContextRef.current.createBufferSource();
-  //     source.buffer = audioBuffers[state];
-  //     source.connect(audioContextRef.current.destination);
-  //     source.onended = () => {
-  //       setTimeout(() => {
-  //         if (!isPlaying) return;
-  //         transitionToNextState();
-  //       }, 0)
-  //     };
-  //     source.start();
-  //   },
-  //   [audioBuffers, transitionToNextState, isPlaying]
-  // );
+		// 音声を再生
+		const source = audioContextRef.current.createBufferSource()
+		source.buffer = audioBuffers[nextState]
+		source.connect(audioContextRef.current.destination)
+		source.start()
+		// 再生終了を待つ
+		await new Promise<void>((resolve) => {
+			source.onended = async () => {
+				await sleep(100)
+				resolve()
+			}
+		})
+		setSteps((prev) => prev + 1)
+	}, [audioBuffers, currentState])
 
-  const next = useCallback(async () => {
-    if (!audioBuffers || !audioContextRef.current) return;
-    const currentIndex = states.indexOf(currentState);
-    let nextStateIndex = 0;
-    const randomValue = Math.random();
-    let cumulativeProbability = 0;
-    for (let i = 0; i < 7; i++) {
-      cumulativeProbability += P[currentIndex][i];
-      if (randomValue < cumulativeProbability) {
-        nextStateIndex = i;
-        break;
-      }
-    }
-    setCurrentState(states[nextStateIndex]);
+	const handleStart = useCallback(async () => {
+		if (isPlaying) return
+		if (!audioContextRef.current) {
+			audioContextRef.current = new AudioContext()
+			await loadAudioFiles()
+		}
+		setIsPlaying(true)
+	}, [isPlaying, loadAudioFiles])
 
-    // 音声を再生
-    const source = audioContextRef.current.createBufferSource();
-    source.buffer = audioBuffers[states[nextStateIndex]];
-    source.connect(audioContextRef.current.destination);
-    source.start();
-    // 再生終了を待つ
-    await new Promise<void>((resolve) => {
-      source.onended = () => resolve();
-    });
-  }, [audioBuffers, currentState]);
+	const handleStop = () => {
+		setIsPlaying(false)
+		if (!audioContextRef.current) return
+		audioContextRef.current = null
+	}
 
-  const startTicks = useCallback(async() => {
-    setTimeout(async () => {
-      if (!isPlaying) return;
-      await next()
-    }, 10)
-    // while (true) {
-    //   if (!isPlaying) break;
-    //   await next()
-    // }
-  }, [isPlaying, next])
+	// biome-ignore lint/correctness/useExhaustiveDependencies: Emit when step changes
+	useEffect(() => {
+		if (!isPlaying) return
+		next()
+	}, [steps, isPlaying])
 
-  const handlePlay = useCallback(() => {
-    if (isPlaying) return;
-    if (!audioContextRef.current) {
-      audioContextRef.current = new AudioContext();
-    }
-    setIsPlaying(true);
-  }, [isPlaying]);
-
-  const handleStop = () => {
-    setIsPlaying(false);
-    if (!audioContextRef.current) return
-    audioContextRef.current.close();
-    audioContextRef.current = null;
-    setCurrentState("b");
-  };
-
-  const init = useCallback(async () => {
-    audioContextRef.current = new AudioContext();
-    await loadAudioFiles();
-    startTicks();
-  }, [loadAudioFiles, startTicks])
-
-  useEffect(() => {
-    init()
-  }, [init]);
-
-  return (
-    <main className="max-w-screen-[1000px] mx-auto text-center">
-      <div className='relative my-6 mx-auto w-[750px] h-[450px]'>
-        <StateNode isCurrent={currentState === "b"} className='top-[50px] left-[175px]'>★</StateNode>
-        <StateNode isCurrent={currentState === "し"} className='top-[175px] left-[325px]'>し</StateNode>
-        <StateNode isCurrent={currentState === "か"} className='top-[50px] left-[475px]'>か</StateNode>
-        <StateNode isCurrent={currentState === "の"} className='top-[175px] left-[625px]'>の</StateNode>
-        <StateNode isCurrent={currentState === "こ"} className='top-[300px] left-[475px]'>こ</StateNode>
-        <StateNode isCurrent={currentState === "た"} className='top-[300px] left-[175px]'>た</StateNode>
-        <StateNode isCurrent={currentState === "ん"} className='top-[175px] left-[25px]'>ん</StateNode>
-      </div>
-      <button
-        type="button"
-        className="bg-red-950 text-white font-bold p-4 text-4xl rounded-lg hover:brightness-150 disabled:bg-stone-500 mr-4"
-        onClick={handlePlay}
-        disabled={isPlaying}
-      >
-        スタート
-      </button>
-      <button
-        type="button"
-        className="bg-gray-900 text-white font-bold p-4 text-4xl rounded-lg hover:brightness-150 disabled:bg-stone-500"
-        onClick={handleStop}
-        disabled={!isPlaying}
-      >
-        ストップ
-      </button>
-    </main>
-  );
-};
+	return (
+		<main className="max-w-screen-[1000px] mx-auto text-center">
+			<div className="relative my-6 mx-auto w-[750px] h-[450px]">
+				<StateNode
+					isCurrent={currentState === "b"}
+					className="top-[50px] left-[175px]"
+				>
+					★
+				</StateNode>
+				<StateNode
+					isCurrent={currentState === "し"}
+					className="top-[175px] left-[325px]"
+				>
+					し
+				</StateNode>
+				<StateNode
+					isCurrent={currentState === "か"}
+					className="top-[50px] left-[475px]"
+				>
+					か
+				</StateNode>
+				<StateNode
+					isCurrent={currentState === "の"}
+					className="top-[175px] left-[625px]"
+				>
+					の
+				</StateNode>
+				<StateNode
+					isCurrent={currentState === "こ"}
+					className="top-[300px] left-[475px]"
+				>
+					こ
+				</StateNode>
+				<StateNode
+					isCurrent={currentState === "た"}
+					className="top-[300px] left-[175px]"
+				>
+					た
+				</StateNode>
+				<StateNode
+					isCurrent={currentState === "ん"}
+					className="top-[175px] left-[25px]"
+				>
+					ん
+				</StateNode>
+			</div>
+			<button
+				type="button"
+				className="bg-red-950 text-white font-bold p-4 text-4xl rounded-lg hover:brightness-150 disabled:bg-stone-500 mr-4"
+				onClick={handleStart}
+				disabled={isPlaying}
+			>
+				スタート
+			</button>
+			<button
+				type="button"
+				className="bg-gray-900 text-white font-bold p-4 text-4xl rounded-lg hover:brightness-150 disabled:bg-stone-500"
+				onClick={handleStop}
+				disabled={!isPlaying}
+			>
+				ストップ
+			</button>
+		</main>
+	)
+}
--- src/StateNode.tsx
+++ src/StateNode.tsx
@@ -1,16 +1,31 @@
-import { FC, PropsWithChildren } from "react";
-import { cn } from "./lib/cn";
+import { FC, PropsWithChildren } from "react"
+import { cn } from "./lib/classname"
 
 type Props = {
-  isCurrent: boolean
-  className?:string
+	isCurrent: boolean
+	className?: string
 }
-export const StateNode: FC<PropsWithChildren<Props>> = ({children, isCurrent, className}) => {
-  return (
-    <div className={cn("bg-white w-[100px] h-[100px] rounded-full border-[10px] border-red-400 absolute", className, !isCurrent && "border-stone-400")}>
-      <div className={cn("flex flex-col w-full h-full justify-center items-center text-stone-800 text-[48px] font-bold", !isCurrent && "text-stone-700")}>
-      {children}
-      </div>
-    </div>
-  )
+export const StateNode: FC<PropsWithChildren<Props>> = ({
+	children,
+	isCurrent,
+	className
+}) => {
+	return (
+		<div
+			className={cn(
+				"bg-white w-[100px] h-[100px] rounded-full border-[10px] border-red-400 absolute",
+				className,
+				!isCurrent && "border-stone-400"
+			)}
+		>
+			<div
+				className={cn(
+					"flex flex-col w-full h-full justify-center items-center text-stone-800 text-[48px] font-bold select-none",
+					!isCurrent && "text-stone-700"
+				)}
+			>
+				{children}
+			</div>
+		</div>
+	)
 }
